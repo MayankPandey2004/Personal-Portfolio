9cbf297898d12453d4b435cf45e2c6fb94499232
MayankPandey2004 <mpandey2004@gmail.com>
2024-07-09 21:23:55 +0530

    Deploated Website

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -49,9 +49,6 @@ function NavBar() {
         <Router>
             <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
                 <Container>
-                    <Navbar.Brand href="/">
-                        <img src={logo} alt="Logo" style={{height:90}}/>
-                    </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav">
                         <span className="navbar-toggler-icon"></span>
                     </Navbar.Toggle>
