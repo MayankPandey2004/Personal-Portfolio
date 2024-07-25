ea9f6eeddcd1819839165cb09c035b2b714e3893
MayankPandey2004 <mpandey2004@gmail.com>
2024-07-25 13:10:51 +0530

    updated projects

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -6,7 +6,7 @@ export default function ProjectCard({ title, description, imgUrl }) {
     return (
         <Col sm={6} md={4}>
             <div className='proj-imgbx'>
-                <img src={imgUrl} />
+                <img src={imgUrl} style={{height:300}}/>
                 <div className='proj-txtx'>
                     <h4>{title}</h4>
                     <span>{description}</span>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,13 +10,13 @@ const Projects = () => {
 
     const projects = [
         {
-            title: "Business Startup",
-            description: "Design & Development",
+            title: "Realtime Chat App",
+            description: "using React Native and Firebase",
             imgUrl: projImg1,
         },
         {
-            title: "Business Startup",
-            description: "Design & Development",
+            title: "AskMe App",
+            description: "using React and GoLang",
             imgUrl: projImg2,
         },
         {
@@ -48,7 +48,7 @@ const Projects = () => {
                     <Col size={12}>
                         <h2>Projects</h2>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                        <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                        <Tab.Container id="projects-tabs" defaultActiveKey="first" >
                             <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                                 <Nav.Item>
                                     <Nav.Link eventKey="first">Tab 1</Nav.Link>
@@ -60,7 +60,7 @@ const Projects = () => {
                                     <Nav.Link eventKey="third">Tab 3</Nav.Link>
                                 </Nav.Item>
                             </Nav>
-                            <Tab.Content id="slideInUp" className={"animate__animated animate__slideInUp"}>
+                            <Tab.Content id="slideInUp" className={"animate__animated animate__slideInUp"} >
                                 <Tab.Pane eventKey="first">
                                     <Row>
                                         {
