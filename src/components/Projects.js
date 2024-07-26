c05888df8de24085ce7f273e17e42a3f1661afa8
MayankPandey2004 <mpandey2004@gmail.com>
2024-07-26 14:55:00 +0530

    updated projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,4 @@
-import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { Container, Row, Col, Tab, } from "react-bootstrap";
 import ProjectCard from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
@@ -46,19 +46,8 @@ const Projects = () => {
                 <Row>
                     <Col size={12}>
                         <h2>Projects</h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        <p>These are some of my recent projects that highlight my diverse skill set in developing responsive web layouts to handling dynamic components in mobile apps. These projects reflect my commitment to continuous learning and innovation in the field of software development.</p>
                         <Tab.Container id="projects-tabs" defaultActiveKey="first" >
-                            <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                                <Nav.Item>
-                                    <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                                </Nav.Item>
-                                <Nav.Item>
-                                    <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                                </Nav.Item>
-                                <Nav.Item>
-                                    <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                                </Nav.Item>
-                            </Nav>
                             <Tab.Content id="slideInUp" className={"animate__animated animate__slideInUp"} >
                                 <Tab.Pane eventKey="first">
                                     <Row>
@@ -73,12 +62,6 @@ const Projects = () => {
                                             })
                                         }
                                     </Row>
-                                </Tab.Pane>
-                                <Tab.Pane eventKey="second">
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                                </Tab.Pane>
-                                <Tab.Pane eventKey="third">
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
                                 </Tab.Pane>
                             </Tab.Content>
                         </Tab.Container>
