909b09958c85954c39737e4abd47cf324381dc27
MayankPandey2004 <mpandey2004@gmail.com>
2024-08-12 16:35:26 +0530

    updated skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,18 +1,25 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable jsx-a11y/img-redundant-alt */
-import React from 'react'
+import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import meter1 from '../assets/img/meter1.svg';
-import meter2 from '../assets/img/meter2.svg';
-import meter3 from '../assets/img/meter3.svg';
-import colorSharp from '../assets/img/color-sharp.png'
+import cppIcon from '../assets/img/cpp-icon.png'; 
+import cIcon from '../assets/img/c-icon.png';    // Example path
+import javaIcon from '../assets/img/java-icon.svg';  // Example path
+import htmlIcon from '../assets/img/html-icon.png';  // Example path
+import cssIcon from '../assets/img/css-icon.png';    // Example path
+import jsIcon from '../assets/img/javascript-icon.png'; // Example path
+import tsIcon from '../assets/img/typescript-icon.png'; // Example path
+import mysqlIcon from '../assets/img/mysql-icon.png'; // Example path
+import reactjsIcon from '../assets/img/reactjs-icon.png'; // Example path
+import reactnativeIcon from '../assets/img/reactnative-icon.svg'; // Example path
+import bootstrapIcon from '../assets/img/bootstrap-icon.svg'; // Example path
+import colorSharp from '../assets/img/color-sharp.png';
 
 export default function Skills() {
     const responsive = {
         superLargeDesktop: {
-            // the naming can be any, depends on you.
             breakpoint: { max: 4000, min: 3000 },
             items: 5
         },
@@ -29,41 +36,68 @@ export default function Skills() {
             items: 1
         }
     };
+
     return (
         <section className='skill' id='skills'>
             <Container style={{paddingTop:200,paddingBottom:100}}>
                 <Row>
                     <Col>
                         <div className='skill-bx'>
-                            <h2>Skills</h2>
-                            <p>Sample Text</p>
+                            <h2 style={{ fontSize: '2.5rem', marginBottom: '1.5rem', fontWeight: 'bold' }}>Skills</h2>
+                            <p style={{ fontSize: '1.1rem', marginBottom: '3rem' }}>
+                                Proficient in various programming languages and development tools, ensuring high-quality software solutions.
+                            </p>
                             <Carousel responsive={responsive} infinite={true} className='skill-slider'>
-                                <div className='item'>
-                                    <img src={meter1} alt='Image' />
-                                    <h5>Web Development</h5>
+                                <div className='item' style={{ padding: '20px', textAlign: 'center' }}>
+                                    <img src={cppIcon} alt='C/C++' style={{ maxWidth: '100px', height: '100px', marginBottom: '20px' }} />
+                                    <h3>C++</h3>
                                 </div>
-                                <div className='item'>
-                                    <img src={meter1} alt='Image' />
-                                    <h5>App Development</h5>
+                                <div className='item' style={{ padding: '20px', textAlign: 'center' }}>
+                                    <img src={cIcon} alt='C/C++' style={{ maxWidth: '100px', height: '100px', marginBottom: '20px' }} />
+                                    <h3>C</h3>
                                 </div>
-                                <div className='item'>
-                                    <img src={meter3} alt='Image' />
-                                    <h5>C & C++</h5>
+                                <div className='item' style={{ padding: '20px', textAlign: 'center' }}>
+                                    <img src={javaIcon} alt='Java' style={{ maxWidth: '100px', height: '100px', marginBottom: '20px' }} />
+                                    <h3>Java</h3>
                                 </div>
-                                <div className='item'>
-                                    <img src={meter2} alt='Image' />
-                                    <h5>Java</h5>
+                                <div className='item' style={{ padding: '20px', textAlign: 'center' }}>
+                                    <img src={htmlIcon} alt='HTML' style={{ maxWidth: '100px', height: '100px', marginBottom: '20px' }} />
+                                    <h3>HTML</h3>
                                 </div>
-                                <div className='item'>
-                                    <img src={meter3} alt='Image' />
-                                    <h5>Python</h5>
+                                <div className='item' style={{ padding: '20px', textAlign: 'center' }}>
+                                    <img src={cssIcon} alt='CSS' style={{ maxWidth: '100px', height: '100px', marginBottom: '20px' }} />
+                                    <h3>CSS</h3>
+                                </div>
+                                <div className='item' style={{ padding: '20px', textAlign: 'center' }}>
+                                    <img src={jsIcon} alt='JavaScript' style={{ maxWidth: '100px', height: '100px', marginBottom: '20px' }} />
+                                    <h3>JavaScript</h3>
+                                </div>
+                                <div className='item' style={{ padding: '20px', textAlign: 'center' }}>
+                                    <img src={tsIcon} alt='TypeScript' style={{ maxWidth: '100px', height: '100px', marginBottom: '20px' }} />
+                                    <h3>TypeScript</h3>
+                                </div>
+                                <div className='item' style={{ padding: '20px', textAlign: 'center' }}>
+                                    <img src={mysqlIcon} alt='MySQL' style={{ maxWidth: '100px', height: '100px', marginBottom: '20px' }} />
+                                    <h3>MySQL</h3>
+                                </div>
+                                <div className='item' style={{ padding: '20px', textAlign: 'center' }}>
+                                    <img src={reactjsIcon} alt='ReactJS' style={{ maxWidth: '100px', height: '100px', marginBottom: '20px' }} />
+                                    <h3>ReactJS</h3>
+                                </div>
+                                <div className='item' style={{ padding: '20px', textAlign: 'center' }}>
+                                    <img src={reactnativeIcon} alt='React Native' style={{ maxWidth: '100px', height: '100px', marginBottom: '20px' }} />
+                                    <h3>React Native</h3>
+                                </div>
+                                <div className='item' style={{ padding: '20px', textAlign: 'center' }}>
+                                    <img src={bootstrapIcon} alt='Bootstrap' style={{ maxWidth: '100px', height: '100px', marginBottom: '20px' }} />
+                                    <h3>Bootstrap</h3>
                                 </div>
                             </Carousel>
                         </div>
                     </Col>
                 </Row>
             </Container>
-            <img className='background-image-left' src={colorSharp} />
+            <img className='background-image-left' src={colorSharp} style={{ position: 'absolute', left: 0, bottom: 0, zIndex: -1, width: '50%', height: 'auto' }} />
         </section>
-    )
+    );
 }
