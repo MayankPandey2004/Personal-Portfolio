535e4a80d71ec5fd2c321803d7f687f7ab86fc65
MayankPandey2004 <mpandey2004@gmail.com>
2025-05-28 00:15:24 +0530

    added projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,8 @@ import ProjectCard from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
+import projImg4 from "../assets/img/project-img4.png";
+
 import colorSharp2 from "../assets/img/color-sharp2.png";
 
 const Projects = () => {
@@ -23,6 +25,22 @@ const Projects = () => {
             description: "using Python and discord.dev",
             imgUrl: projImg3,
         },
+        {
+            title: "Discord Music Bot",
+            description: "using Python and discord.dev",
+            imgUrl: projImg3,
+        },
+        {
+            title: "Discord Music Bot",
+            description: "using Python and discord.dev",
+            imgUrl: projImg3,
+        },
+        {
+            title: "Discord Music Bot",
+            description: "using Python and discord.dev",
+            imgUrl: projImg3,
+        },
+
         
     ];
 
